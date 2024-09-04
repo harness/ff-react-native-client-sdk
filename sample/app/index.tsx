fc4a27667e2ce5f447b595a231271146f181cf3a
Erdi Rowlands <erdirowlands@gmail.com>
2024-09-04 13:06:16 +0100

    FFM-11972 Add sample expo app

---
+++ sample/app/index.tsx
@@ -0,0 +1,58 @@
+import { StyleSheet, Text, View } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+
+import {
+  FFContextProvider,
+  useFeatureFlag,
+  useFeatureFlags
+} from '@harnessio/ff-react-native-client-sdk'
+
+export default function App() {
+  return (
+      <View style={styles.container}>
+        <FFContextProvider
+            apiKey=""
+            target={{
+              identifier: 'target1', // <- replace with an identifier unique to the user, e.g. email or UUID
+              name: 'target1' // <- replace with a name unique t o the user
+            }}
+            async={true}
+            options={{cache: true, debug: true, streamEnabled: false, pollingEnabled: true}}
+        >
+
+          { <MultipleFeatureFlags />}
+        </FFContextProvider>
+
+        <StatusBar style="auto" />
+      </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'orange',
+    alignItems: 'center',
+    justifyContent: 'center',
+    minHeight: '100%'
+  }
+})
+
+function SingleFeatureFlag() {
+  const flagValue = useFeatureFlags(['harnessappdemodarkmode', 'harnessappdemodarkmode','harnessappdemodarkmode','harnessappdemodarkmode','harnessappdemodarkmode'])
+
+  return (
+      <Text>The value of "harnessappdemodarkmode" is {JSON.stringify(flagValue)}</Text>
+  )
+}
+
+function MultipleFeatureFlags() {
+  const flags = useFeatureFlags(['boolflagwith12groups', 'stringflagwith12groups','numberflagnogrouprule','jsonflagnogrouprule','numberflag'])
+
+  return (
+      <>
+        <Text>Here are all our flags:</Text>
+        <Text>{JSON.stringify(flags, null, 2)}</Text>
+      </>
+  )
+}
